9b0502259bae70cdda6ef9453e54f9b61bc92b2d
HungryApe9847 <rowanliamjones@gmail.com>
2026-04-02 12:59:36 +0100

    Add Pelican site, initial post and IDE configs
    Add a Pelican-based site under my-vercel-site (pelicanconf.py, publishconf.py), initial post (content/post1.md), Vercel deployment config (vercel.json) and a site-specific .gitignore. Also add IDE project files (.idea/*) including module, inspection profiles, VCS mappings and misc settings to set up the development environment.

---
+++ my-vercel-site/content/post1.md
@@ -0,0 +1,13 @@
+title: Hellooooo!
+date: 2026-04-02
+author: Rowan
+category: blog
+tags: first, lol
+slug: 1
+# Hello.
+
+## This is my first post.
+
+I have recently acquired a domain, so I may as well do something with it. I have never had a domain so this will be interesting.
+## As a warning...
+This is pretty much a learning experience for me, so they will be a few Godot games here. I am absolutely not a very advanced programmer (yet) 😈
---
+++ my-vercel-site/pelicanconf.py
@@ -0,0 +1,36 @@
+AUTHOR = 'Rowan'
+SITENAME = 'Blog.RowanCodes'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'En'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+THEME = "pelican-themes/bootstrap2-dark"
+
+# Blogroll
+LINKS = (
+    ("Main Site", "https://rowancodes.dev/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 15
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
